4bca41495521b1a4e5c5abea48c60e438cabf30e
sjeffry-o <kronizzyd@gmail.com>
2022-05-03 11:52:03 +0300

    pandas dumping every N steps

--- wikipedia_parse.py
+++ wikipedia_parse.py
@@ -1,6 +1,7 @@
 import requests as req
 from bs4 import BeautifulSoup
 from PIL import Image
+import pandas as pd
 import re
 
 #object_methods = [method_name for method_name in dir(WikiExtractor)
@@ -32,12 +33,18 @@ def indexByWikiObjTextKey(list, key):
     return index
 
 wiki_url = "https://ru.wikipedia.org"
-url = "https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B_%D0%BF%D0%BE_%D1%85%D1%83%D0%B4%D0%BE%D0%B6%D0%BD%D0%B8%D0%BA%D0%B0%D0%BC"
-url = "https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B_%D0%BF%D0%BE_%D1%85%D1%83%D0%B4%D0%BE%D0%B6%D0%BD%D0%B8%D0%BA%D0%B0%D0%BC&subcatfrom=%D0%9A%D0%BB%D0%B5%D0%B5%2C+%D0%9F%D0%B0%D1%83%D0%BB%D1%8C%0A%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B+%D0%9F%D0%B0%D1%83%D0%BB%D1%8F+%D0%9A%D0%BB%D0%B5%D0%B5#mw-subcategories"
+#url = "https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B_%D0%BF%D0%BE_%D1%85%D1%83%D0%B4%D0%BE%D0%B6%D0%BD%D0%B8%D0%BA%D0%B0%D0%BC"
+#url = "https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B_%D0%BF%D0%BE_%D1%85%D1%83%D0%B4%D0%BE%D0%B6%D0%BD%D0%B8%D0%BA%D0%B0%D0%BC&subcatfrom=%D0%9A%D0%BB%D0%B5%D0%B5%2C+%D0%9F%D0%B0%D1%83%D0%BB%D1%8C%0A%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B+%D0%9F%D0%B0%D1%83%D0%BB%D1%8F+%D0%9A%D0%BB%D0%B5%D0%B5#mw-subcategories"
+url = "https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B_%D0%BF%D0%BE_%D1%85%D1%83%D0%B4%D0%BE%D0%B6%D0%BD%D0%B8%D0%BA%D0%B0%D0%BC&subcatfrom=%D0%A1%D0%B8%D0%BD%D1%8C%D0%B5%D0%B8-%D0%9C%D0%B5%D1%80%D1%88%D0%B5%0A%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B+%D0%9F%D0%B0%D0%BB%D0%B0+%D0%A1%D0%B8%D0%BD%D1%8C%D0%B5%D0%B8-%D0%9C%D0%B5%D1%80%D1%88%D0%B5#mw-subcategories"
 
 links = reqParseFind(url, 'a')
-links_slice = links[10:15]
+links_slice = links[4:-6]
 
+#print([a.text for a in links_slice])
+
+count = 0
+im_descriptions = []
+im_names = []
 for artist_link in links_slice:
     artist_FULLurl = url + artist_link['href']
     paintingsListLinks = reqParseFind(artist_FULLurl, 'a')
@@ -53,24 +60,34 @@ for artist_link in links_slice:
             categ_index = indexByWikiObjTextKey(paintingsLinks, 'Категории')
             paintingsLinks = paintingsLinks[:categ_index]
             filteredPaintingLinks = []
+
             for paintingLink in paintingsLinks:
                 if paintingLink['href'].find("/wiki/") != -1\
                     and "Список" not in paintingLink.text and "Медиафайлы" not in paintingLink.text:
                     filteredPaintingLinks.append(paintingLink)
+
             for paintingLink in filteredPaintingLinks:
                 painting_FULLurl = wiki_url + paintingLink['href']
                 text = reqParseFind(painting_FULLurl, 'p')
-                text = [cleanhtml(p.text) for p in text]
+                text = ' '.join([cleanhtml(p.text) for p in text[:6]])
                 imgs = reqParseFind(painting_FULLurl, 'img')
                 img_url = 'https:' + imgs[0]['src']
                 resp = req.get(img_url, stream=True)
+
                 if len(resp.content) > 3000:
                     im_name = imgs[0]['alt'].replace('.', '').replace(' ', '')
-                    with open(f'{im_name}.jpg', 'wb') as f:
+                    im_path = f'imgs/{im_name}.jpg'
+                    with open(im_path, 'wb') as f:
                         f.write(resp.content)
+
+                    im_descriptions.append(text)
+                    im_names.append(im_name)
+                    if (count + 1) % 100 == 0:
+                        data = {'im_name': im_names, 'description': im_descriptions}
+                        pd.DataFrame.from_dict(data).to_csv('wikiArtistsImDescr.csv', index=False)
+                        print(f'saved {count} iteration')
+                    count += 1
 
                 #print(img_url, '\n', len(resp.content))
                 #print(imgs[0]['src'][2:])
                 #print([img['src'] for img in imgs[0]])
-        print()
-    break
