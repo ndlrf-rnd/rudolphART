1eb304ff772a621b7b0aa57c8247872e66eae7b0
sjeffry-o <kronizzyd@gmail.com>
2022-05-03 08:40:16 +0300

    filtering by id BodyContent; using nonUsefulLinks

--- wikipedia_parse.py
+++ wikipedia_parse.py
@@ -2,6 +2,9 @@ import requests as req
 from bs4 import BeautifulSoup
 import pickle
 
+with open('nonUsefulWikiLinksTexts.pickle', 'rb') as f:
+    nonUsefulLinksTexts = pickle.load(f)
+
 wiki_url = "https://ru.wikipedia.org"
 url = "https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B_%D0%BF%D0%BE_%D1%85%D1%83%D0%B4%D0%BE%D0%B6%D0%BD%D0%B8%D0%BA%D0%B0%D0%BC"
 url = "https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B_%D0%BF%D0%BE_%D1%85%D1%83%D0%B4%D0%BE%D0%B6%D0%BD%D0%B8%D0%BA%D0%B0%D0%BC&subcatfrom=%D0%9A%D0%BB%D0%B5%D0%B5%2C+%D0%9F%D0%B0%D1%83%D0%BB%D1%8C%0A%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D1%8B+%D0%9F%D0%B0%D1%83%D0%BB%D1%8F+%D0%9A%D0%BB%D0%B5%D0%B5#mw-subcategories"
@@ -9,12 +12,12 @@ url = "https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3
 def reqParseFind(url, findTag):
     resp = req.get(url)
     soup = BeautifulSoup(resp.text, 'html.parser')
-    links = soup.findAll(findTag)
+    links = soup.find(id="bodyContent").findAll(findTag)
     return links
 
 links = reqParseFind(url, 'a')
-
 links_slice = links[10:15]
+
 for artist_link in links_slice:
     artist_FULLurl = url + artist_link['href']
     paintingsListLinks = reqParseFind(artist_FULLurl, 'a')
@@ -26,11 +29,8 @@ for artist_link in links_slice:
             #print(painting_link)
             paintings_FULLurl = wiki_url + paintingListLink['href']
             paintingsLinks = reqParseFind(paintings_FULLurl, 'a')
-            start_urls = [url.text for url in paintingsLinks[:5]]
-            end_urls = [url.text for url in paintingsLinks[-60:]]
-            nonUsefulLinksTexts = start_urls + end_urls
-            print(nonUsefulLinksTexts)
-            with open('nonUsefulWikiLinksTexts.pickle', 'wb') as f:
-                pickle.dump(nonUsefulLinksTexts, f)
-            break
+            for paintingLink in paintingsLinks:
+                if paintingLink.text not in nonUsefulLinksTexts:
+                    print(paintingLink.text)
+        print()
     break
